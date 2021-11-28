43cb30b4bc92ec5f7e7c8d82826f02f4cb8c62c4
Alren John L. Mesia <alrenjohnmesias@gmail.com>
2021-11-28 22:26:50 +0800

    Initial Commit

---
+++ resources/js/views/Index.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="wrapper">
+    <parallax class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            <div class="brand">
+              <h1>Japan Travel</h1>
+              <h3>Tokyo | Yokohama | Kyoto | Osaka | Sapporo | Nagoya</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section section-basic">
+        <div class="container">
+          <div class="title">
+            <h2>Discover more...</h2>
+          </div>
+          <country-information></country-information>
+        </div>
+      </div>
+
+        <div class="section section-basic">
+        <div class="container">
+          <div class="title">
+            <h2>Discover more...</h2>
+          </div>
+          <home-carousel></home-carousel>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountryInformation from "./components/HomeCountryInfo";
+import HomeCarousel from "./components/HomeCarouselsection";
+import { LoginCard } from "../components";
+
+export default {
+  components: {
+    CountryInformation,
+    HomeCarousel
+  },
+  name: "index",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("../../assets/img/japan_travel_bg.webp")
+    },
+    signup: {
+      type: String,
+      default: require("../../assets/img/city.jpg")
+    },
+    landing: {
+      type: String,
+      default: require("../../assets/img/landing.jpg")
+    },
+    profile: {
+      type: String,
+      default: require("../../assets/img/profile.jpg")
+    }
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false
+    };
+  },
+  methods: {
+    leafActive() {
+      if (window.innerWidth < 768) {
+        this.leafShow = false;
+      } else {
+        this.leafShow = true;
+      }
+    }
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      };
+    },
+    signupImage() {
+      return {
+        backgroundImage: `url(${this.signup})`
+      };
+    }
+  },
+  mounted() {
+    this.leafActive();
+    window.addEventListener("resize", this.leafActive);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.leafActive);
+  }
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+</style>
---
+++ resources/js/views/components/HomeCountryInfo.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="wrapper">
+    <div id="buttons">
+      <div class="md-layout">
+          <div
+          class="md-layout-item md-size-25 md-xsmall-size-100 md-small-size-50 md-medium-size-25"
+        >
+          <md-field>
+            <md-input v-model="initial" placeholder="Regular"></md-input>
+          </md-field>
+        </div>
+      </div>
+
+
+    </div>
+    <!-- end buttons -->
+  
+  </div>
+</template>
+
+<script>
+import { Pagination } from "../../components";
+import { Slider } from "../../components";
+import { Badge } from "../../components";
+
+export default {
+  components: {
+    Pagination,
+    Slider,
+    Badge
+  },
+  data() {
+    return {
+      defaultPagination: 3,
+      infoPagination: 3,
+      initial: null,
+      floatingLabel: null,
+      success: null,
+      error: null,
+      withMIcon: null,
+      withFaIcon: null,
+      checkbox1: true,
+      checkbox2: null,
+      checkbox3: true,
+      checkbox4: null,
+      radio1: true,
+      radio2: false,
+      switch1: true,
+      switch2: null,
+      amount: 30,
+      amount2: 60,
+      buffer: 40,
+      sliders: {
+        simple: 40,
+        rangeSlider: [20, 60]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.vertical-center {
+  display: flex;
+  align-items: center;
+}
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
+.md-checkbox,
+.md-radio {
+  display: flex;
+  margin: 0;
+  margin-bottom: 0.5rem;
+}
+</style>
